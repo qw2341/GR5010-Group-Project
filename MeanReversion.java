6f90d978bafcbd56b6958d81fffb447bdf9ebbc6
Qingqi Wu <qw2341@tc.columbia.edu>
2022-04-15 20:35:17 -0400

    fixed some major issue

--- MeanReversion.java
+++ MeanReversion.java
@@ -30,12 +30,16 @@ public class MeanReversion {
     double startingBal = 100.00*tickers.size();
     double endingBal= 0.00;
     
+  //broker commission fee
+    double commFee = 0.0;
+    
+    
     //System.out.println("Size: "+tickers.size()+ " With weight: "+ weight);
     //for each stock
     for (String ticker: tickers) {
       //assuming equal weights
       
-      double y = (double) invest(data.stocks, ticker, 30, 1, 100, true, 0.0);
+      double y = (double) invest(data.stocks, ticker, 20, 2, 100, true, 0.0, commFee);
       //System.out.println("Adding Yield: "+y+ " With weight: "+ weight);
       endingBal += y;
       
@@ -60,7 +64,7 @@ public class MeanReversion {
    * @param investAmount percentage of portfolio to invest 1-invest everything; 0.5-invest half; 0-invest in only 1 share
    * @return
    */
-  private static double invest(HashMap<String, Double[]> stocks,String ticker, int n, int k, double money, boolean allowShort, double investAmount) {
+  private static double invest(HashMap<String, Double[]> stocks,String ticker, int n, int k, double money, boolean allowShort, double investAmount, double commFee) {
     double bgBal = money;
     Double[] prices = stocks.get(ticker);
     
@@ -77,18 +81,20 @@ public class MeanReversion {
       days ++;
       //compute moving average
       if(i>=n-1 && prices[i-n+1]>=0) {
-        double maSum = 0, maStdev = 0;
-        for(int j=i-n+1;j<i;j++) {
-          maSum+= prices[j];
+        
+        double maSum = 0.0, maStdev = 0.0;
+        
+        //calculate the moving average
+        for(int j=i-n+1;j<i+1;j++) {
+          maSum += prices[j];
         }
-        //its mean now
-        maSum/=n;
+        maSum = (double) maSum / (double) n;
         
         //get Stdev
-        for(int j=i-n+1;j<i;j++) {
+        for(int j=i-n+1;j<i+1;j++) {
           maStdev += Math.pow(prices[j] - maSum, 2);
         }
-        maStdev = Math.sqrt(maStdev/n);
+        maStdev = Math.sqrt(maStdev/(double)n);
         
         //this is for exporting purposes
         //Double[] result = new Double[2];
@@ -96,28 +102,35 @@ public class MeanReversion {
         //result[1] = maStdev;
         
         //get z score
-        double z = (prices[i] - maSum) / maStdev;
+        double z = (double) (prices[i] - maSum) / maStdev;
         
         //how much stock to buy / sell
         int pos;
-        if (investAmount <= 0.0001) pos = 1;
-        else pos = (int) Math.floor((money/prices[i])*investAmount);// invest half
-         //only but/sell one share
+        if (investAmount <= 0.0001) pos = 1;//only but/sell one share
+        else pos = (int) Math.floor((money/prices[i])*investAmount);// invest custom amount
         
+        //amount of money to invest
+        double amount = prices[i]*pos;
         
+        //System.out.printf("On day %d, average of %d days is %f, with stdev of %f, and a z-score of %f %n", days, k, maSum, maStdev, z);
         //invest decisions
         if(z>k && allowShort) {
           //short if z>1
-          money += prices[i]*pos;
+          money += amount;
+          money -= amount*commFee;
           position -= pos;
-        } else if (z<k) {
-          //buy if z>1
-          money -= prices[i]*pos;
+          //System.out.printf("Shorting %d shares, gaining $%f%n",pos,amount);
+        } else if (z<-k) {
+          //buy if z<-1
+          money -= amount;
           position += pos;
-          
+          money -= amount*commFee;
+          //System.out.printf("Buying %d shares, costing $%f%n",pos,amount);
         } else if (Math.abs(z) <0.5) {
           //close if near moving average
           money += position*prices[i];
+          //System.out.printf("Closing Position of %d shares, gaining/spending $%f%n",position,position*prices[i]);
+          money -= amount*commFee;
           position = 0;
         }
         
@@ -133,7 +146,13 @@ public class MeanReversion {
     return money;
   }
   
-  //t is the number of years
+  /**
+   * Calculates annual growth rate
+   * @param bgBal beginning balance
+   * @param endBal ending bal
+   * @param t number of years
+   * @return
+   */
   private static double cagr(double bgBal, double endBal, double t) {
     return Math.pow(endBal/bgBal, 1/t)-1;
   }
